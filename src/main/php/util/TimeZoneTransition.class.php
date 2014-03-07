b262e2ac4b2ed71c0e6a73f7d9160ddcc42f6fdb
Timm Friebe <thekid@thekid.de>
2014-03-07 17:07:48 +0100

    MFB: Refrain from using deprecated timezone abbreviations Fixes last two parts of PHP 5.6 Parity - see xp-framework/xp-framework#352 See also https://bugs.php.net/bug.php?id=66845

--- src/main/php/util/TimeZoneTransition.class.php
+++ src/main/php/util/TimeZoneTransition.class.php
@@ -86,7 +86,7 @@ class TimeZoneTransition extends \lang\Object {
     }
     if (!isset($t)) throw new \lang\IllegalArgumentException('Timezone '.$this->tz->getName().' does not have DST transitions.');
 
-    $this->date= new Date($last['ts'], new TimeZone($last['abbr']));
+    $this->date= new Date($last['ts'], $this->date->getTimeZone());
     $this->isDst= $last['isdst'];
     $this->offset= $last['offset'];
     $this->abbr= $last['abbr'];
