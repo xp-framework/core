a8381c361617cf7a09f544213ca36e56cb6a8159
Timm Friebe <thekid@thekid.de>
2014-08-31 22:23:31 +0200

    Make __xp.php mostly consistent with future __xp.php files inside packages See https://github.com/xp-framework/rfc/issues/287#issuecomment-53993975

--- src/main/php/lang.base.php
+++ src/main/php/lang.base.php
@@ -625,3 +625,42 @@ class import {
   }
 }
 // }}}
+
+// {{{ main
+date_default_timezone_set(ini_get('date.timezone')) || xp::error('[xp::core] date.timezone not configured properly.');
+
+define('LONG_MAX', PHP_INT_MAX);
+define('LONG_MIN', -PHP_INT_MAX - 1);
+define('MODIFIER_STATIC',       1);
+define('MODIFIER_ABSTRACT',     2);
+define('MODIFIER_FINAL',        4);
+define('MODIFIER_PUBLIC',     256);
+define('MODIFIER_PROTECTED',  512);
+define('MODIFIER_PRIVATE',   1024);
+
+error_reporting(E_ALL);
+set_error_handler('__error');
+
+global $paths;
+if (!isset($paths)) $paths= array(__DIR__.DIRECTORY_SEPARATOR, '.'.DIRECTORY_SEPARATOR);
+xp::$null= new null();
+xp::$loader= new xp();
+xp::$classpath= $paths;
+set_include_path(rtrim(implode(PATH_SEPARATOR, $paths), PATH_SEPARATOR));
+
+spl_autoload_register(function($class) {
+  $name= strtr($class, '\\', '.');
+  $cl= xp::$loader->findClass($name);
+  if ($cl instanceof null) return false;
+  $cl->loadClass0($name);
+  return true;
+});
+spl_autoload_register(function($class) {
+  if (false === strrpos($class, '\\import')) {
+    return false;
+  } else {
+    class_alias('import', $class);
+    return true;
+  }
+});
+// }}}
