16a8fd309d61e379822cffcec266ce5eeed28f58
Timm Friebe <thekid@thekid.de>
2014-09-04 23:51:39 +0200

    Next try: Release

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,8 +4,9 @@ REL=$1
 
 cd src/main/php
 echo $REL > VERSION
-xar cvf ../../../xp-rt-$REL.xar . VERSION
+./xar cvf ../../../xp-rt-$REL.xar . VERSION
 cd ../../..
 
 cat glue.json | sed -e "s/.version.*:.*,/\"version\" : \"$REL\",/g" > tmp ; mv tmp glue.json
-ls -al xp-rt-$REL.xar glue.json
+ls -al xp-rt-$REL.xar
+cat glue.json
