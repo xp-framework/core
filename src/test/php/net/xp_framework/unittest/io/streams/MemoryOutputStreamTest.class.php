4859633e5338f0d7100b2fba781f1f5e17c7efea
Timm Friebe <thekid@thekid.de>
2015-01-06 23:32:11 +0100

    QA: Adjust to new unittest coding standards

--- src/test/php/net/xp_framework/unittest/io/streams/MemoryOutputStreamTest.class.php
+++ src/test/php/net/xp_framework/unittest/io/streams/MemoryOutputStreamTest.class.php
@@ -1,51 +1,35 @@
 <?php namespace net\xp_framework\unittest\io\streams;
 
-use unittest\TestCase;
 use io\streams\MemoryOutputStream;
-
 
 /**
  * Unit tests for streams API
  *
- * @see      xp://io.streams.OutputStream
- * @purpose  Unit test
+ * @see   xp://io.streams.OutputStream
+ * @see   xp://lang.Closeable#close
  */
-class MemoryOutputStreamTest extends TestCase {
-  protected $out= null;
+class MemoryOutputStreamTest extends \unittest\TestCase {
+  private $out;
 
   /**
    * Setup method. Creates the fixture.
-   *
    */
   public function setUp() {
     $this->out= new MemoryOutputStream();
   }
 
-  /**
-   * Test string writing
-   *
-   */
   #[@test]
-  public function writeString() {
+  public function writing_a_string() {
     $this->out->write('Hello');
     $this->assertEquals('Hello', $this->out->getBytes());
   }
 
-  /**
-   * Test number writing
-   *
-   */
   #[@test]
-  public function writeNumber() {
+  public function writing_a_number() {
     $this->out->write(5);
     $this->assertEquals('5', $this->out->getBytes());
   }
 
-  /**
-   * Test closing a stream twice has no effect.
-   *
-   * @see   xp://lang.Closeable#close
-   */
   #[@test]
   public function closingTwice() {
     $this->out->close();
