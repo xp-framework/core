3c11bcc6accf126d69646ff2ab5363cc6881eedd
Timm Friebe <thekid@thekid.de>
2015-01-06 23:52:18 +0100

    Make io.streams.MemoryOutputStream implement io.streams.Seekable

--- src/main/php/io/streams/MemoryOutputStream.class.php
+++ src/main/php/io/streams/MemoryOutputStream.class.php
@@ -5,7 +5,8 @@
  *
  * @test  xp://net.xp_framework.unittest.io.streams.MemoryOutputStreamTest
  */
-class MemoryOutputStream extends \lang\Object implements OutputStream {
+class MemoryOutputStream extends \lang\Object implements OutputStream, Seekable {
+  protected $pos= 0;
   protected $bytes= '';
   
   /**
@@ -13,8 +14,14 @@ class MemoryOutputStream extends \lang\Object implements OutputStream {
    *
    * @param  var $arg
    */
-  public function write($arg) { 
-    $this->bytes.= $arg;
+  public function write($arg) {
+    $l= strlen($arg);
+    if ($this->pos < strlen($this->bytes)) {
+      $this->bytes= substr_replace($this->bytes, $arg, $this->pos, $l);
+    } else {
+      $this->bytes.= $arg;
+    }
+    $this->pos+= $l;
   }
 
   /**
@@ -24,7 +31,31 @@ class MemoryOutputStream extends \lang\Object implements OutputStream {
    */
   public function flush() { 
   }
-  
+
+  /**
+   * Seek to a given offset
+   *
+   * @param   int $offset
+   * @param   int $whence default SEEK_SET (one of SEEK_[SET|CUR|END])
+   * @throws  io.IOException in case of error
+   */
+  public function seek($offset, $whence= SEEK_SET) {
+    switch ($whence) {
+      case SEEK_SET: $this->pos= $offset; break;
+      case SEEK_CUR: $this->pos+= $offset; break;
+      case SEEK_END: $this->pos= strlen($this->bytes) + $offset; break;
+    }
+  }
+
+  /**
+   * Return current offset
+   *
+   * @return  int
+   */
+  public function tell() {
+    return $this->pos;
+  }
+
   /**
    * Retrieve stored bytes
    *
--- src/test/php/net/xp_framework/unittest/io/streams/MemoryOutputStreamTest.class.php
+++ src/test/php/net/xp_framework/unittest/io/streams/MemoryOutputStreamTest.class.php
@@ -31,7 +31,60 @@ class MemoryOutputStreamTest extends \unittest\TestCase {
   }
 
   #[@test]
-  public function closingTwice() {
+  public function tell() {
+    $this->out->write('Hello');
+    $this->assertEquals(5, $this->out->tell());
+  }
+
+  #[@test, @values([0, 1, 5])]
+  public function tell_after_seeking_to_beginning_plus($offset) {
+    $this->out->write('Hello');
+    $this->out->seek($offset, SEEK_SET);
+    $this->assertEquals($offset, $this->out->tell());
+  }
+
+  #[@test, @values([0, 1, 5])]
+  public function tell_after_seeking_to_end_minus($offset) {
+    $this->out->write('Hello');
+    $this->out->seek(-$offset, SEEK_END);
+    $this->assertEquals(5 - $offset, $this->out->tell());
+  }
+
+  #[@test]
+  public function replacing_character() {
+    $this->out->write('Hello');
+    $this->out->seek(0, SEEK_SET);
+    $this->out->write('h');
+    $this->assertEquals('hello', $this->out->getBytes());
+  }
+
+  #[@test]
+  public function seeking_to_end() {
+    $this->out->write('Hello');
+    $this->out->seek(0, SEEK_END);
+    $this->out->write('!');
+    $this->assertEquals('Hello!', $this->out->getBytes());
+  }
+
+  #[@test]
+  public function seeking_to_end_minus_one() {
+    $this->out->write('Hello');
+    $this->out->seek(-1, SEEK_END);
+    $this->out->write('_');
+    $this->assertEquals('Hell_', $this->out->getBytes());
+  }
+
+  #[@test]
+  public function overwriting() {
+    $this->out->write('Hello');
+    $this->out->seek(1, SEEK_SET);
+    $this->out->write('ai');
+    $this->out->write('fisch');
+    $this->assertEquals('Haifisch', $this->out->getBytes());
+  }
+
+  #[@test]
+  public function closing_twice_has_no_effect() {
     $this->out->close();
     $this->out->close();
   }
