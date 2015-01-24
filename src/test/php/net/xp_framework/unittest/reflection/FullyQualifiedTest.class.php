10d8d8e98aab445915b947af2629ebe06c8e3868
Timm Friebe <thekid@thekid.de>
2015-01-24 13:43:46 +0100

    Fix interfaceImplemented() test by using getDeclaredInterfaces()

--- src/test/php/net/xp_framework/unittest/reflection/FullyQualifiedTest.class.php
+++ src/test/php/net/xp_framework/unittest/reflection/FullyQualifiedTest.class.php
@@ -5,27 +5,25 @@ use lang\archive\Archive;
 use io\File;
 use util\Date;
 use lang\ResourceProvider;
+use lang\ClassLoader;
+use lang\archive\ArchiveClassLoader;
+use lang\XPClass;
 
 /**
  * Tests RFC #0037
  *
- * @see      rfc://0037
- * @purpose  Unit test
+ * @see    rfc://0037
  */
 class FullyQualifiedTest extends TestCase {
 
   static function __static() {
-    \lang\ClassLoader::registerLoader(new \lang\archive\ArchiveClassLoader(
-      new Archive(\lang\XPClass::forName(\xp::nameOf(__CLASS__))->getPackage()->getPackage('lib')->getResourceAsStream('fqcns.xar'))
+    ClassLoader::registerLoader(new ArchiveClassLoader(
+      new Archive(XPClass::forName(\xp::nameOf(__CLASS__))->getPackage()->getPackage('lib')->getResourceAsStream('fqcns.xar'))
     ));
-    \lang\XPClass::forName('info.binford6100.Date');
-    \lang\XPClass::forName('de.thekid.util.ObjectComparator');
+    XPClass::forName('info.binford6100.Date');
+    XPClass::forName('de.thekid.util.ObjectComparator');
   }
 
-  /**
-   * Tests that util.Date and info.binford6100.Date can coexist
-   *
-   */
   #[@test]
   public function dateClassesCanCoexist() {
     $bd= new \info·binford6100·Date();
@@ -35,43 +33,26 @@ class FullyQualifiedTest extends TestCase {
     $this->assertEquals('util.Date', $ud->getClassName());
   }
 
-  /**
-   * Tests that XPClass::forName() returns distinct class
-   * objects for the util.Date and info.binford6100.Date
-   * classes.
-   *
-   */
   #[@test]
   public function classObjectsAreNotEqual() {
-    $bc= \lang\XPClass::forName('info.binford6100.Date');
-    $uc= \lang\XPClass::forName('util.Date');
+    $bc= XPClass::forName('info.binford6100.Date');
+    $uc= XPClass::forName('util.Date');
     $this->assertNotEquals($bc, $uc);
   }
 
-  /**
-   * Tests that XPClass::forName() loads fully qualified classes 
-   * correctly.
-   *
-   */
   #[@test]
   public function dynamicallyLoaded() {
-    $class= \lang\XPClass::forName('de.thekid.List');
+    $class= XPClass::forName('de.thekid.List');
     $this->assertEquals('de.thekid.List', $class->getName());
     $instance= $class->newInstance();
     $this->assertEquals('de.thekid.List@{}', $instance->toString());
   }
 
-  /**
-   * Tests that XPClass::forName() loads fully qualified classes 
-   * correctly.
-   *
-   */
   #[@test]
   public function interfaceImplemented() {
-    $class= \lang\XPClass::forName('de.thekid.util.ObjectComparator');
-    $interfaces= $class->getInterfaces();
-    $this->assertEquals(2, sizeof($interfaces));
-    $this->assertEquals('lang.Generic', $interfaces[0]->getName());
-    $this->assertEquals('de.thekid.util.Comparator', $interfaces[1]->getName());
+    $this->assertEquals(
+      [XPClass::forName('de.thekid.util.Comparator')],
+      XPClass::forName('de.thekid.util.ObjectComparator')->getDeclaredInterfaces()
+    );
   }
 }
