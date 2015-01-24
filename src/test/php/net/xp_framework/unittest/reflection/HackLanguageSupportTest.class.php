d9ff34a9c74e19c859d22f04907c1740514394d5
Timm Friebe <thekid@thekid.de>
2015-01-24 20:42:43 +0100

    Move hack language tests to separate class

---
+++ src/test/php/net/xp_framework/unittest/reflection/HackLanguageSupportTest.class.php
@@ -0,0 +1,40 @@
+<?php namespace net\xp_framework\unittest\reflection;
+
+use lang\Type;
+use lang\Primitive;
+use lang\ArrayType;
+
+/**
+ * TestCase for HACK language feature support
+ *
+ * @see    http://docs.hhvm.com/manual/en/hack.annotations.types.php
+ * @see    xp://lang.Type
+ */
+class HackLanguageSupportTest extends \unittest\TestCase {
+
+  
+  #[@test]
+  public function string_type() {
+    $this->assertEquals(Primitive::$STRING, Type::forName('HH\string'));
+  }
+
+  #[@test]
+  public function int_type() {
+    $this->assertEquals(Primitive::$INT, Type::forName('HH\int'));
+  }
+
+  #[@test]
+  public function double_type() {
+    $this->assertEquals(Primitive::$DOUBLE, Type::forName('HH\float'));
+  }
+
+  #[@test]
+  public function bool_type() {
+    $this->assertEquals(Primitive::$BOOL, Type::forName('HH\bool'));
+  }
+
+  #[@test]
+  public function array_of_string_type() {
+    $this->assertEquals(new ArrayType('string'), Type::forName('array<HH\string>'));
+  }
+}
