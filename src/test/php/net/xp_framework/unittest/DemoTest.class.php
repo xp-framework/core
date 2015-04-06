283d935c81335b7b0e7626cb8d6fda3281a2a65e
Timm Friebe <thekid@thekid.de>
2015-04-06 21:25:03 +0200

    Adjust to new coding standards; use usleep() instead of loop

--- src/test/php/net/xp_framework/unittest/DemoTest.class.php
+++ src/test/php/net/xp_framework/unittest/DemoTest.class.php
@@ -1,33 +1,17 @@
-<?php namespace net\framework\unittest;
+<?php namespace net\xp_framework\unittest;
+
+use lang\IllegalArgumentException;
+use unittest\PrerequisitesNotMetError;
 
 /**
  * Shows different test scenarios
- *
- * @purpose  Unit Test
  */
 class DemoTest extends \unittest\TestCase {
 
   /**
-   * A test that always succeeds
-   *
-   */
-  #[@test]
-  public function alwaysSucceeds() {
-    $this->assertTrue(true);
-  }
-
-  /**
-   * A test that is ignored
-   *
-   */
-  #[@test, @ignore('Ignored')]
-  public function ignored() {
-    $this->fail('Ignored test executed', 'executed', 'ignored');
-  }
-
-  /**
    * Setup method
    *
+   * @return void
    */
   public function setUp() {
     if ('alwaysSkipped' === $this->name) {
@@ -35,53 +19,38 @@ class DemoTest extends \unittest\TestCase {
     }
   }
 
-  /**
-   * A test that is skipped due to not met prerequisites.
-   *
-   */
+  #[@test]
+  public function alwaysSucceeds() {
+    $this->assertTrue(true);
+  }
+
+  #[@test, @ignore('Ignored')]
+  public function ignored() {
+    $this->fail('Ignored test executed', 'executed', 'ignored');
+  }
+
   #[@test]
   public function alwaysSkipped() {
     $this->fail('Skipped test executed', 'executed', 'skipped');
   }
 
-  /**
-   * A test that always fails because of a failed assertion
-   *
-   */
   #[@test]
   public function alwaysFails() {
     $this->assertTrue(false);
   }
 
-  /**
-   * A test that always fails because the expected exception was not
-   * thrown.
-   *
-   */
-  #[@test, @expect('lang.IllegalArgumentException')]
+  #[@test, @expect(IllegalArgumentException::class)]
   public function expectedExceptionNotThrown() {
-    true;
+    // Intentionally empty
   }
 
-  /**
-   * A test that always fails because an exception is thrown.
-   *
-   */
   #[@test]
   public function throwsAnException() {
     throw new IllegalArgumentException('');
   }
 
-  /**
-   * A test that timeouts
-   *
-   */
   #[@test, @limit(time= 0.1)]
   public function timeouts() {
-    $start= gettimeofday();
-    $end= (1000000 * $start['sec']) + $start['usec'] + 1000 * 200;    // 0.2 seconds
-    do {
-      $now= gettimeofday();
-    } while ((1000000 * $now['sec']) + $now['usec'] < $end);
+    usleep(200000);
   }
 }
