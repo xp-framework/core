57d3f3f71624091dad3d0bfc7d62ea3b31798558
Timm Friebe <thekid@thekid.de>
2015-04-06 20:53:50 +0200

    Migrate to namespaces

--- src/test/php/net/xp_framework/unittest/DemoTest.class.php
+++ src/test/php/net/xp_framework/unittest/DemoTest.class.php
@@ -1,94 +1,87 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace net\framework\unittest;
+
+/**
+ * Shows different test scenarios
  *
- * $Id$
+ * @purpose  Unit Test
  */
- 
-  uses('unittest.TestCase');
+class DemoTest extends \unittest\TestCase {
 
   /**
-   * Shows different test scenarios
+   * A test that always succeeds
    *
-   * @purpose  Unit Test
    */
-  class DemoTest extends TestCase {
-      
-    /**
-     * A test that always succeeds
-     *
-     */
-    #[@test]
-    public function alwaysSucceeds() {
-      $this->assertTrue(TRUE);
-    }
+  #[@test]
+  public function alwaysSucceeds() {
+    $this->assertTrue(true);
+  }
 
-    /**
-     * A test that is ignored
-     *
-     */
-    #[@test, @ignore('Ignored')]
-    public function ignored() {
-      $this->fail('Ignored test executed', 'executed', 'ignored');
-    }
+  /**
+   * A test that is ignored
+   *
+   */
+  #[@test, @ignore('Ignored')]
+  public function ignored() {
+    $this->fail('Ignored test executed', 'executed', 'ignored');
+  }
 
-    /**
-     * Setup method
-     *
-     */
-    public function setUp() {
-      if (0 == strcasecmp('alwaysSkipped', $this->name)) {
-        throw new PrerequisitesNotMetError('Skipping', null, $this->name);
-      }
-    }
-
-    /**
-     * A test that is skipped due to not met prerequisites.
-     *
-     */
-    #[@test]
-    public function alwaysSkipped() {
-      $this->fail('Skipped test executed', 'executed', 'skipped');
-    }
-
-    /**
-     * A test that always fails because of a failed assertion
-     *
-     */
-    #[@test]
-    public function alwaysFails() {
-      $this->assertTrue(FALSE);
-    }
-
-    /**
-     * A test that always fails because the expected exception was not
-     * thrown.
-     *
-     */
-    #[@test, @expect('lang.IllegalArgumentException')]
-    public function expectedExceptionNotThrown() {
-      TRUE;
-    }
-
-    /**
-     * A test that always fails because an exception is thrown.
-     *
-     */
-    #[@test]
-    public function throwsAnException() {
-      throw new IllegalArgumentException('');
-    }
-
-    /**
-     * A test that timeouts
-     *
-     */
-    #[@test, @limit(time= 0.1)]
-    public function timeouts() {
-      $start= gettimeofday();
-      $end= (1000000 * $start['sec']) + $start['usec'] + 1000 * 200;    // 0.2 seconds
-      do {
-        $now= gettimeofday();
-      } while ((1000000 * $now['sec']) + $now['usec'] < $end);
+  /**
+   * Setup method
+   *
+   */
+  public function setUp() {
+    if ('alwaysSkipped' === $this->name) {
+      throw new PrerequisitesNotMetError('Skipping', null, $this->name);
     }
   }
-?>
+
+  /**
+   * A test that is skipped due to not met prerequisites.
+   *
+   */
+  #[@test]
+  public function alwaysSkipped() {
+    $this->fail('Skipped test executed', 'executed', 'skipped');
+  }
+
+  /**
+   * A test that always fails because of a failed assertion
+   *
+   */
+  #[@test]
+  public function alwaysFails() {
+    $this->assertTrue(false);
+  }
+
+  /**
+   * A test that always fails because the expected exception was not
+   * thrown.
+   *
+   */
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function expectedExceptionNotThrown() {
+    true;
+  }
+
+  /**
+   * A test that always fails because an exception is thrown.
+   *
+   */
+  #[@test]
+  public function throwsAnException() {
+    throw new IllegalArgumentException('');
+  }
+
+  /**
+   * A test that timeouts
+   *
+   */
+  #[@test, @limit(time= 0.1)]
+  public function timeouts() {
+    $start= gettimeofday();
+    $end= (1000000 * $start['sec']) + $start['usec'] + 1000 * 200;    // 0.2 seconds
+    do {
+      $now= gettimeofday();
+    } while ((1000000 * $now['sec']) + $now['usec'] < $end);
+  }
+}
