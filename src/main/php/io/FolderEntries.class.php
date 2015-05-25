979352defe0b8083553d54828832cf16e7846577
Timm Friebe <thekid@thekid.de>
2015-05-25 13:49:36 +0200

    Add FolderEntries::named() method

---
+++ src/main/php/io/FolderEntries.class.php
@@ -0,0 +1,78 @@
+<?php namespace io;
+
+use lang\IllegalArgumentException;
+
+/**
+ * Folder entries provi
+ *
+ * @test  xp://net.xp_framework.unittest.io.FolderEntriesTest
+ * @see   xp://io.Folder#entries
+ */
+class FolderEntries extends \lang\Object implements \Iterator {
+  const EOF = -1;
+  private $base, $entry;
+  private $handle= null;
+
+  /**
+   * Creates a new Folder entries object
+   *
+   * @param  var... $args Either an io.Folder, an io.Path or a string
+   * @throws lang.IllegalArgumentException
+   */
+  public function __construct() {
+    $this->base= Path::compose(func_get_args());
+    if ($this->base->isEmpty()) {
+      throw new IllegalArgumentException('Cannot create from empty name');
+    }
+  }
+
+  /**
+   * Returns a path by a given name inside this folder
+   *
+   * @param  string $name
+   * @return io.Path
+   */
+  public function named($name) {
+    return new Path($this->base, $name);
+  }
+
+  /** @return string */
+  public function current() { return new Path($this->base, $this->entry); }
+
+  /** @return string */
+  public function key() { return $this->entry; }
+
+  /** @return bool */
+  public function valid() { return false !== $this->entry; }
+
+  /** @return void */
+  public function next() {
+    do {
+      $entry= readdir($this->handle);
+    } while ('.' === $entry || '..' === $entry);
+    $this->entry= $entry;
+  }
+
+  /** @return void */
+  public function rewind() {
+    if (null === $this->handle) {
+      if (!is_resource($handle= opendir($this->base->asFolder()->getURI()))) {
+        $e= new IOException('Cannot open folder '.$this->base);
+        \xp::gc(__FILE__);
+        throw $e;
+      }
+      $this->handle= $handle;
+    } else {
+      rewinddir($this->handle);
+    }
+    $this->next();
+  }
+
+  /** @return void */
+  public function __destruct() {
+    if (null !== $this->handle) {
+      closedir($this->handle);
+      $this->handle= null;
+    }
+  }
+}
---
+++ src/test/php/net/xp_framework/unittest/io/FolderEntriesTest.class.php
@@ -0,0 +1,106 @@
+<?php namespace net\xp_framework\unittest\io;
+
+use io\Folder;
+use io\Path;
+use io\File;
+use io\FolderEntries;
+use lang\System;
+use lang\IllegalArgumentException;
+
+class FolderEntriesTest extends \unittest\TestCase {
+  private $folder;
+
+  /**
+   * Sets up test case - initializes temp directory in %TEMP%
+   *
+   * @return void
+   */
+  public function setUp() {
+    $this->folder= new Folder(System::tempDir(), md5(uniqid()).'.xp');
+    $this->folder->exists() && $this->folder->unlink();
+    $this->folder->create();
+  }
+
+  /**
+   * Deletes directory in %TEMP% (including any files inside) if existant
+   *
+   * @return void
+   */
+  public function tearDown() {
+    $this->folder->exists() && $this->folder->unlink();
+  }
+
+  #[@test]
+  public function can_create_with_folder() {
+    new FolderEntries($this->folder);
+  }
+
+  #[@test]
+  public function can_create_with_uri() {
+    new FolderEntries($this->folder->getURI());
+  }
+
+  #[@test]
+  public function can_create_with_reference_to_current_directory() {
+    new FolderEntries('.');
+  }
+
+  #[@test]
+  public function can_create_with_reference_to_parent_directory() {
+    new FolderEntries('..');
+  }
+
+  #[@test]
+  public function can_create_with_path() {
+    new FolderEntries(new Path($this->folder));
+  }
+
+  #[@test, @expect(IllegalArgumentException::class), @values([null, ''])]
+  public function cannot_create_from_empty_name($value) {
+    new FolderEntries($value);
+  }
+
+  #[@test]
+  public function entries_iteration_for_empty_folder() {
+    $this->assertEquals([], iterator_to_array(new FolderEntries($this->folder)));
+  }
+
+  #[@test]
+  public function entries_iteration_with_one_file() {
+    (new File($this->folder, 'one'))->touch();
+
+    $this->assertEquals(
+      ['one' => new Path($this->folder, 'one')],
+      iterator_to_array(new FolderEntries($this->folder))
+    );
+  }
+
+  #[@test]
+  public function entries_iteration_with_files_and_directories() {
+    (new File($this->folder, 'one'))->touch();
+    (new Folder($this->folder, 'two'))->create();
+
+    $this->assertEquals(
+      ['one' => new Path($this->folder, 'one'), 'two' => new Path($this->folder, 'two')],
+      iterator_to_array(new FolderEntries($this->folder))
+    );
+  }
+
+  #[@test]
+  public function entries_reiteration() {
+    (new File($this->folder, 'one'))->touch();
+    (new File($this->folder, 'two'))->touch();
+
+    $expected= ['one' => new Path($this->folder, 'one'), 'two' => new Path($this->folder, 'two')];
+    $entries= new FolderEntries($this->folder);
+    $this->assertEquals(
+      [$expected, $expected],
+      [iterator_to_array($entries), iterator_to_array($entries)]
+    );
+  }
+
+  #[@test]
+  public function named() {
+    $this->assertEquals(new Path($this->folder, 'test'), (new FolderEntries($this->folder))->named('test'));
+  }
+}
