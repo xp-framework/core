043961b160819b7db2fe1828b982cfef0242dc36
Timm Friebe <thekid@thekid.de>
2015-05-31 17:53:42 +0200

    Refrain from using the lang.types.String class

--- src/test/php/net/xp_framework/unittest/util/collections/HashSetTest.class.php
+++ src/test/php/net/xp_framework/unittest/util/collections/HashSetTest.class.php
@@ -1,15 +1,13 @@
 <?php namespace net\xp_framework\unittest\util\collections;
  
-use unittest\TestCase;
 use util\collections\HashSet;
-use lang\types\String;
 
 /**
  * Test HashSet class
  *
  * @see   xp://util.collections.HashSet
  */
-class HashSetTest extends TestCase {
+class HashSetTest extends \unittest\TestCase {
   public $set= null;
   
   /**
@@ -26,36 +24,36 @@ class HashSetTest extends TestCase {
 
   #[@test]
   public function equalsClone() {
-    $this->set->add(new String('green'));
+    $this->set->add(new Name('green'));
     $this->assertTrue($this->set->equals(clone($this->set)));
   }
  
   #[@test]
   public function equalsOtherSetWithSameContents() {
     $other= new HashSet();
-    $this->set->add(new String('color'));
-    $other->add(new String('color'));
+    $this->set->add(new Name('color'));
+    $other->add(new Name('color'));
     $this->assertTrue($this->set->equals($other));
   }
 
   #[@test]
   public function doesNotEqualSetWithDifferentContents() {
     $other= new HashSet();
-    $this->set->add(new String('blue'));
-    $other->add(new String('yellow'));
+    $this->set->add(new Name('blue'));
+    $other->add(new Name('yellow'));
     $this->assertFalse($this->set->equals($other));
   }
  
   #[@test]
   public function add() {
-    $this->set->add(new String('green'));
+    $this->set->add(new Name('green'));
     $this->assertFalse($this->set->isEmpty());
     $this->assertEquals(1, $this->set->size());
   }
 
   #[@test]
   public function addAll() {
-    $array= array(new String('one'), new String('two'), new String('three'));
+    $array= array(new Name('one'), new Name('two'), new Name('three'));
     $this->set->addAll($array);
     $this->assertFalse($this->set->isEmpty());
     $this->assertEquals(3, $this->set->size());
@@ -63,63 +61,63 @@ class HashSetTest extends TestCase {
 
   #[@test]
   public function addAllUniques() {
-    $array= array(new String('one'), new String('one'), new String('two'));
+    $array= array(new Name('one'), new Name('one'), new Name('two'));
     $this->set->addAll($array);
     $this->assertFalse($this->set->isEmpty());
-    $this->assertEquals(2, $this->set->size());   // String{"one"} and String{"two"}
+    $this->assertEquals(2, $this->set->size());   // Name{"one"} and Name{"two"}
   }
 
   #[@test]
   public function addAllReturnsWhetherSetHasChanged() {
-    $array= array(new String('caffeine'), new String('nicotine'));
+    $array= array(new Name('caffeine'), new Name('nicotine'));
     $this->assertTrue($this->set->addAll($array));
     $this->assertFalse($this->set->addAll($array));
-    $this->assertFalse($this->set->addAll(array(new String('caffeine'))));
+    $this->assertFalse($this->set->addAll(array(new Name('caffeine'))));
     $this->assertFalse($this->set->addAll([]));
   }
 
   #[@test]
   public function contains() {
-    $this->set->add(new String('key'));
-    $this->assertTrue($this->set->contains(new String('key')));
-    $this->assertFalse($this->set->contains(new String('non-existant-key')));
+    $this->set->add(new Name('key'));
+    $this->assertTrue($this->set->contains(new Name('key')));
+    $this->assertFalse($this->set->contains(new Name('non-existant-key')));
   }
 
   #[@test]
   public function addSameValueTwice() {
-    $color= new String('green');
+    $color= new Name('green');
     $this->assertTrue($this->set->add($color));
     $this->assertFalse($this->set->add($color));
   }
 
   #[@test]
   public function remove() {
-    $this->set->add(new String('key'));
-    $this->assertTrue($this->set->remove(new String('key')));
+    $this->set->add(new Name('key'));
+    $this->assertTrue($this->set->remove(new Name('key')));
     $this->assertTrue($this->set->isEmpty());
   }
 
   #[@test]
   public function removeOnEmptySet() {
-    $this->assertFalse($this->set->remove(new String('irrelevant-set-is-empty-anyway')));
+    $this->assertFalse($this->set->remove(new Name('irrelevant-set-is-empty-anyway')));
   }
 
   #[@test]
   public function removeNonExistantObject() {
-    $this->set->add(new String('key'));
-    $this->assertFalse($this->set->remove(new String('non-existant-key')));
+    $this->set->add(new Name('key'));
+    $this->assertFalse($this->set->remove(new Name('non-existant-key')));
   }
 
   #[@test]
   public function clear() {
-    $this->set->add(new String('key'));
+    $this->set->add(new Name('key'));
     $this->set->clear();
     $this->assertTrue($this->set->isEmpty());
   }
 
   #[@test]
   public function toArray() {
-    $color= new String('red');
+    $color= new Name('red');
     $this->set->add($color);
     $this->assertEquals(array($color), $this->set->toArray());
   }
@@ -131,19 +129,19 @@ class HashSetTest extends TestCase {
 
   #[@test]
   public function iteration() {
-    $this->set->add(new String('1'));
-    $this->set->add(new String('2'));
-    $this->set->add(new String('3'));
+    $this->set->add(new Name('1'));
+    $this->set->add(new Name('2'));
+    $this->set->add(new Name('3'));
     
     foreach ($this->set as $i => $value) {
-      $this->assertEquals(new String($i+ 1), $value);
+      $this->assertEquals(new Name($i+ 1), $value);
     }
   }
 
   #[@test]
   public function stringRepresentation() {
-    $this->set->add(new String('color'));
-    $this->set->add(new String('price'));
+    $this->set->add(new Name('color'));
+    $this->set->add(new Name('price'));
     $this->assertEquals(
       "util.collections.HashSet[2] {\n  color,\n  price\n}",
       $this->set->toString()
