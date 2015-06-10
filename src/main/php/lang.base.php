c7d1221e4169bb6f1e99620a0c56d9a842968e88
Timm Friebe <thekid@thekid.de>
2015-06-10 09:53:33 +0200

    Adjust is() and xp::typeOf() to renaming

--- src/main/php/lang.base.php
+++ src/main/php/lang.base.php
@@ -56,12 +56,12 @@ final class xp {
   public static $cll= 0;
   public static $cl= [];
   public static $cn= [
-    'xp'    => '<xp>',
-    'null'  => '<null>'
+    'xp'     => '<xp>',
+    '__null' => '<null>'
   ];
   public static $sn= [
     'xp'     => 'xp',
-    'null'   => 'null',
+    'null'   => '__null',
     'string' => "\xfestring",
     'int'    => "\xfeint",
     'double' => "\xfedouble",
@@ -160,7 +160,7 @@ final class xp {
       return $arg ? 'true' : 'false';
     } else if (is_null($arg)) {
       return 'null';
-    } else if ($arg instanceof null) {
+    } else if ($arg instanceof __null) {
       return '<null>';
     } else if (is_int($arg) || is_float($arg)) {
       return (string)$arg;
@@ -439,7 +439,7 @@ function cast($arg, $type, $nullsafe= true) {
 //     Checks whether a given object is an instance of the type given
 function is($type, $object) {
   if (null === $type) {
-    return $object instanceof null;
+    return $object instanceof __null;
   } else if ('int' === $type) {
     return is_int($object);
   } else if ('double' === $type) {
