2717ffc4fe46119ea9fddd4d2ec00f83942110bc
Timm Friebe <thekid@thekid.de>
2015-06-13 14:04:39 +0200

    Make isInstance() tests compatible with PHP7

--- src/test/php/net/xp_framework/unittest/reflection/IsInstanceTest.class.php
+++ src/test/php/net/xp_framework/unittest/reflection/IsInstanceTest.class.php
@@ -1,86 +1,53 @@
 <?php namespace net\xp_framework\unittest\reflection;
 
-use unittest\TestCase;
-
+use lang\XPClass;
 
 /**
  * TestCase
  *
  * @see      xp://lang.XPClass#isInstance
  */
-class IsInstanceTest extends TestCase {
+class IsInstanceTest extends \unittest\TestCase {
 
-  /**
-   * Tests this is a unittest.TestCase
-   *
-   */
   #[@test]
   public function thisIsATestCase() {
-    $this->assertTrue(\lang\XPClass::forName('unittest.TestCase')->isInstance($this));
+    $this->assertTrue(XPClass::forName('unittest.TestCase')->isInstance($this));
   }
 
-  /**
-   * Tests this is a unittest.TestCase
-   *
-   */
   #[@test]
   public function thisIsAnInstanceOfThisClass() {
     $this->assertTrue($this->getClass()->isInstance($this));
   }
  
-  /**
-   * Tests a primitive string is not a lang.types.String
-   *
-   */
   #[@test]
-  public function stringIsNotAString() {
-    $this->assertFalse(\lang\XPClass::forName('lang.types.String')->isInstance('Hello'));
+  public function primitiveStringIsNotAValue() {
+    $this->assertFalse(XPClass::forName('lang.Value')->isInstance('Hello'));
   }
 
-  /**
-   * Tests lang.Object is not a lang.types.String
-   *
-   */
   #[@test]
-  public function objectIsNotAString() {
-    $this->assertFalse(\lang\XPClass::forName('lang.types.String')->isInstance(new \lang\Object()));
+  public function objectIsNotAValue() {
+    $this->assertFalse(XPClass::forName('lang.Value')->isInstance(new \lang\Object()));
   }
 
-  /**
-   * Tests lang.Object is a lang.Generic (in fact, any object is)
-   *
-   */
   #[@test]
   public function objectIsAGeneric() {
-    $this->assertTrue(\lang\XPClass::forName('lang.Generic')->isInstance(new \lang\Object()));
+    $this->assertTrue(XPClass::forName('lang.Generic')->isInstance(new \lang\Object()));
   }
 
-  /**
-   * Tests lang.Throwable is a lang.Generic (in fact, any object is)
-   *
-   */
   #[@test]
   public function throwableIsAGeneric() {
-    $this->assertTrue(\lang\XPClass::forName('lang.Generic')->isInstance(new \lang\Throwable('')));
+    $this->assertTrue(XPClass::forName('lang.Generic')->isInstance(new \lang\Throwable('')));
   }
 
-  /**
-   * Tests an anonymous instance created is of its interface type
-   *
-   */
   #[@test]
   public function newInterfaceInstanceIsRunnable() {
-    $this->assertTrue(\lang\XPClass::forName('lang.Runnable')->isInstance(newinstance('lang.Runnable', [], '{
+    $this->assertTrue(XPClass::forName('lang.Runnable')->isInstance(newinstance('lang.Runnable', [], '{
       public function run() { }
     }')));
   }
 
-  /**
-   * Tests NULL is not instanceof an object
-   *
-   */
   #[@test]
   public function nullIsNotAnObject() {
-    $this->assertFalse(\lang\XPClass::forName('lang.Generic')->isInstance(null));
+    $this->assertFalse(XPClass::forName('lang.Generic')->isInstance(null));
   }
 }
