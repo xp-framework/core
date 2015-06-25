25883800d903eb11d8e2ad48ee89ff3cb1870297
Timm Friebe <thekid@thekid.de>
2015-06-25 10:35:05 +0200

    Goodbye \TypeException, welcome \TypeError

--- src/test/php/net/xp_framework/unittest/util/PropertyManagerTest.class.php
+++ src/test/php/net/xp_framework/unittest/util/PropertyManagerTest.class.php
@@ -272,8 +272,9 @@ key="overwritten value"'));
     $fixture= $this->fixture();
     try {
       $fixture->setSources(array($one, null));
-      $this->fail('No exception thrown', null, 'TypeException');
-    } catch (\TypeException $expected) {
+      $this->fail('No exception thrown', null, 'TypeException or TypeError');
+    } catch (\TypeException $expected) {   // PHP 7 alpha1
+    } catch (\TypeError  $expected) {      // PHP 7-dev w/ Throwable -> https://github.com/php/php-src/pull/1284
     }
 
     $this->assertEquals([], $fixture->getSources());
