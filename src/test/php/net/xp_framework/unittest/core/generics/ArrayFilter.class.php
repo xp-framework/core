c314275c786795599e18eb1699087779952f7d94
Timm Friebe <thekid@thekid.de>
2015-07-12 11:45:27 +0200

    Convert to namespaces

--- src/test/php/net/xp_framework/unittest/core/generics/ArrayFilter.class.php
+++ src/test/php/net/xp_framework/unittest/core/generics/ArrayFilter.class.php
@@ -1,56 +1,48 @@
-<?php
-/* This class is part of the XP framework
- *
- * $Id$
- */
+<?php namespace net\xp_framework\unittest\core\generics;
 
-  $package= 'net.xp_framework.unittest.core.generics';
+/**
+ * Generic array filter
+ *
+ * ```php
+ * // Set up filter as anonymous class
+ * $webmethods= create('new ArrayFilter<Method>', [], '{
+ *   protected function accept($method) {
+ *     return $method->hasAnnotation("webmethod");
+ *   }
+ * }');
+ *
+ * // Iterate over filtered array
+ * foreach ($webmethods->filter($class->getMethods()) as $method) {
+ *   Console::writeLine("Webmethod: ", $method);
+ * }
+ * ```
+ */
+#[@generic(self= 'T')]
+abstract class ArrayFilter extends \lang\Object {
+  
+  /**
+   * Accept method - called for each element in the specified list.
+   * Return TRUE if the passed element should be included in the
+   * filtered list, FALSE otherwise
+   *
+   * @param   T element
+   * @return  bool
+   */
+  #[@generic(params= 'T')]
+  protected abstract function accept($element);
 
   /**
-   * Generic array filter
+   * Filter a list of elements
    *
-   * Example:
-   * <code>
-   *   // Set up filter as anonymous class
-   *   $webmethods= create('new ArrayFilter<Method>', [], '{
-   *     protected function accept($method) {
-   *       return $method->hasAnnotation("webmethod");
-   *     }
-   *   }');
-   *
-   *   // Iterate over filtered array
-   *   foreach ($webmethods->filter($class->getMethods()) as $method) {
-   *     Console::writeLine("Webmethod: ", $method);
-   *   }
-   * </code>
+   * @param   T[] elements
+   * @return  T[] filtered
    */
-  #[@generic(self= 'T')]
-  abstract class net·xp_framework·unittest·core·generics·ArrayFilter extends Object {
-    
-    /**
-     * Accept method - called for each element in the specified list.
-     * Return TRUE if the passed element should be included in the
-     * filtered list, FALSE otherwise
-     *
-     * @param   T element
-     * @return  bool
-     */
-    #[@generic(params= 'T')]
-    protected abstract function accept($element);
-
-    /**
-     * Filter a list of elements
-     *
-     * @param   T[] elements
-     * @return  T[] filtered
-     */
-    #[@generic(params= 'T[]', return= 'T[]')]
-    public function filter($elements) {
-      $filtered= [];
-      foreach ($elements as $element) {
-        if ($this->accept($element)) $filtered[]= $element;
-      }
-      return $filtered;
+  #[@generic(params= 'T[]', return= 'T[]')]
+  public function filter($elements) {
+    $filtered= [];
+    foreach ($elements as $element) {
+      if ($this->accept($element)) $filtered[]= $element;
     }
+    return $filtered;
   }
-?>
+}
