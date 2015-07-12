044b44db3f68bf0a900b27f2c869609a30ee2f0c
Timm Friebe <thekid@thekid.de>
2015-07-12 21:28:08 +0200

    Refactor to use new xp.runtime.Code class

--- src/main/php/xp/runtime/Dump.class.php
+++ src/main/php/xp/runtime/Dump.class.php
@@ -5,10 +5,9 @@ use lang\XPClass;
 
 /**
  * Evaluates code and dumps its output.
- *
  */
 class Dump extends \lang\Object {
-  
+
   /**
    * Main
    *
@@ -19,21 +18,15 @@ class Dump extends \lang\Object {
 
     // Read sourcecode from STDIN if no further argument is given
     if (0 === sizeof($args)) {
-      $src= file_get_contents('php://stdin');
+      $code= new Code(file_get_contents('php://stdin'));
     } else {
-      $src= $args[0];
+      $code= new Code($args[0]);
     }
-    $src= trim($src, ' ;').';';
-    
-    // Allow missing return
-    strstr($src, 'return ') || strstr($src, 'return;') || $src= 'return '.$src;
-
-    // Rewrite argc, argv
-    $argv= [XPClass::nameOf(__CLASS__)] + $args;
-    $argc= sizeof($argv);
 
     // Perform
-    $return= eval($src);
+    $argv= [XPClass::nameOf(__CLASS__)] + $args;
+    $argc= sizeof($argv);
+    $return= eval($code->head().$code->expression());
     switch ($way) {
       case '-w': Console::writeLine($return); break;
       case '-d': var_dump($return); break;
