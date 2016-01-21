c26cf72717e41f8bb720a5a21766a01ee2d7c786
Timm Friebe <thekid@thekid.de>
2016-01-21 21:54:14 +0100

    Remove deprecated boxing, unboxing and access to wrapper types

--- src/main/php/lang/Primitive.class.php
+++ src/main/php/lang/Primitive.class.php
@@ -26,61 +26,6 @@ class Primitive extends Type {
   }
   
   /**
-   * Returns the wrapper class for this primitive
-   *
-   * @deprecated Wrapper types will move to their own library
-   * @see     http://en.wikipedia.org/wiki/Wrapper_class
-   * @return  lang.XPClass
-   */
-  public function wrapperClass() {
-    switch ($this) {
-      case self::$STRING: return XPClass::forName('lang.types.String');
-      case self::$INT: return XPClass::forName('lang.types.Integer');
-      case self::$DOUBLE: return XPClass::forName('lang.types.Double');
-      case self::$BOOL: return XPClass::forName('lang.types.Boolean');
-    }
-  }
-  
-  /**
-   * Boxes a type - that is, turns Generics into primitives
-   *
-   * @deprecated Wrapper types will move to their own library
-   * @param   var in
-   * @return  var the primitive if not already primitive
-   * @throws  lang.IllegalArgumentException in case in cannot be unboxed.
-   */
-  public static function unboxed($in) {
-    if ($in instanceof \lang\types\String) return $in->toString();
-    if ($in instanceof \lang\types\Double) return $in->doubleValue();
-    if ($in instanceof \lang\types\Integer) return $in->intValue();
-    if ($in instanceof \lang\types\Boolean) return $in->value;
-    if ($in instanceof \lang\types\ArrayList) return $in->values;   // deprecated
-    if ($in instanceof Generic) {
-      throw new IllegalArgumentException('Cannot unbox '.\xp::typeOf($in));
-    }
-    return $in; // Already primitive
-  }
-
-  /**
-   * Boxes a type - that is, turns primitives into Generics
-   *
-   * @deprecated Wrapper types will move to their own library
-   * @param   var in
-   * @return  lang.Generic the Generic if not already generic
-   * @throws  lang.IllegalArgumentException in case in cannot be boxed.
-   */
-  public static function boxed($in) {
-    if (null === $in || $in instanceof Generic) return $in;
-    $t= gettype($in);
-    if ('string' === $t) return new \lang\types\String($in);
-    if ('integer' === $t) return new \lang\types\Integer($in);
-    if ('double' === $t) return new \lang\types\Double($in);
-    if ('boolean' === $t) return new \lang\types\Boolean($in);
-    if ('array' === $t) return \lang\types\ArrayList::newInstance($in);   // deprecated
-    throw new IllegalArgumentException('Cannot box '.\xp::typeOf($in));
-  }
-  
-  /**
    * Get a type instance for a given name
    *
    * @param   string name
@@ -129,33 +74,10 @@ class Primitive extends Type {
    */
   protected function coerce($value, $default) {
     if (!is_array($value)) switch ($this) {
-      case self::$STRING:
-        if ($value instanceof \lang\types\String) return $value->toString();
-        if ($value instanceof \lang\types\Number) return (string)$value->value;
-        if ($value instanceof \lang\types\Boolean) return (string)$value->value;
-        if ($value instanceof \lang\types\Generic) return $value->toString();
-        return (string)$value;
-
-      case self::$INT:
-        if ($value instanceof \lang\types\String) return (int)$value->toString();
-        if ($value instanceof \lang\types\Number) return $value->intValue();
-        if ($value instanceof \lang\types\Boolean) return (int)$value->value;
-        if ($value instanceof \lang\types\Generic) return (int)$value->toString();
-        return (int)$value;
-
-      case self::$DOUBLE:
-        if ($value instanceof \lang\types\String) return (double)$value->toString();
-        if ($value instanceof \lang\types\Number) return $value->doubleValue();
-        if ($value instanceof \lang\types\Boolean) return (double)$value->value;
-        if ($value instanceof \lang\types\Generic) return (double)$value->toString();
-        return (double)$value;
-
-      case self::$BOOL:
-        if ($value instanceof \lang\types\String) return (bool)$value->toString();
-        if ($value instanceof \lang\types\Number) return (bool)$value->value;
-        if ($value instanceof \lang\types\Boolean) return $value->value;
-        if ($value instanceof \lang\types\Generic) return (bool)$value->toString();
-        return (bool)$value;
+      case self::$STRING: return (string)$value;
+      case self::$INT: return (int)$value;
+      case self::$DOUBLE: return (double)$value;
+      case self::$BOOL: return (bool)$value;
     }
 
     return $default($value);
