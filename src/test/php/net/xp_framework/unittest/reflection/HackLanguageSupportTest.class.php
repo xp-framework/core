760f1c230ceec1a1f127cb1da30a1d24973ad521
Timm Friebe <thekid@thekid.de>
2016-01-24 17:11:03 +0100

    Only check for "noreturn" return type in HHVM >= 3.7.0
    See https://github.com/facebook/hhvm/commit/6f53596fd9d1f1cca20a5e83bc8738cd31898372

--- src/test/php/net/xp_framework/unittest/reflection/HackLanguageSupportTest.class.php
+++ src/test/php/net/xp_framework/unittest/reflection/HackLanguageSupportTest.class.php
@@ -93,7 +93,7 @@ class HackLanguageSupportTest extends \unittest\TestCase {
     $this->assertEquals(Type::$VOID, $this->testClass()->getMethod('returnsNothing')->getReturnType());
   }
 
-  #[@test]
+  #[@test, @action(new VerifyThat(function() { return version_compare(HHVM_VERSION, '3.7.0', 'ge'); }))]
   public function method_noreturn_type() {
     $this->assertEquals(Type::$VOID, $this->testClass()->getMethod('returnsNoreturn')->getReturnType());
   }
