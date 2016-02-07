b8ade4067b75f0bb642c021e3febe49c42a217ef
Timm Friebe <thekid@thekid.de>
2016-02-07 17:06:19 +0100

    Refactor runtime type definition to a trait

--- src/test/php/net/xp_framework/unittest/reflection/MethodsTest.class.php
+++ src/test/php/net/xp_framework/unittest/reflection/MethodsTest.class.php
@@ -1,33 +1,7 @@
 <?php namespace net\xp_framework\unittest\reflection;
 
-use lang\Object;
-use lang\Value;
-use lang\ClassLoader;
-
 abstract class MethodsTest extends \unittest\TestCase {
-  private static $fixtures= [];
-
-  /**
-   * Defines an anonymous type
-   *
-   * @param  string $decl Type declaration
-   * @param  int $modifiers
-   * @return lang.XPClass
-   */
-  protected function type($decl= null, $modifiers= '') {
-    if (!isset(self::$fixtures[$decl])) {
-      $definition= [
-        'modifiers'  => $modifiers,
-        'kind'       => 'class',
-        'extends'    => [Object::class],
-        'implements' => [],
-        'use'        => [CompareTo::class],
-        'imports'    => [Value::class => null]
-      ];
-      self::$fixtures[$decl]= ClassLoader::defineType(self::class.sizeof(self::$fixtures), $definition, $decl);
-    }
-    return self::$fixtures[$decl];
-  }
+  use TypeDefinition;
 
   /**
    * Defines a method inside an anonymous type
---
+++ src/test/php/net/xp_framework/unittest/reflection/TypeDefinition.class.php
@@ -0,0 +1,31 @@
+<?php namespace net\xp_framework\unittest\reflection;
+
+use lang\Object;
+use lang\Value;
+use lang\ClassLoader;
+
+trait TypeDefinition {
+  private static $fixtures= [];
+
+  /**
+   * Defines an anonymous type
+   *
+   * @param  string $decl Type declaration
+   * @param  int $modifiers
+   * @return lang.XPClass
+   */
+  protected function type($decl= null, $modifiers= '') {
+    if (!isset(self::$fixtures[$decl])) {
+      $definition= [
+        'modifiers'  => $modifiers,
+        'kind'       => 'class',
+        'extends'    => [Object::class],
+        'implements' => [],
+        'use'        => [CompareTo::class],
+        'imports'    => [Value::class => null]
+      ];
+      self::$fixtures[$decl]= ClassLoader::defineType(get_class($this).sizeof(self::$fixtures), $definition, $decl);
+    }
+    return self::$fixtures[$decl];
+  }
+}
