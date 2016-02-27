b0e516c55ae579470a0a6cd6a1d03eb0c9022eb6
Timm Friebe <thekid@thekid.de>
2016-02-27 13:58:10 +0100

    Simplify implementation by using `yield` statement

--- src/main/php/io/streams/LinesIn.class.php
+++ src/main/php/io/streams/LinesIn.class.php
@@ -7,11 +7,10 @@ use lang\IllegalArgumentException;
  * by either Unix, Mac or Windows line endings.
  *
  * @see   xp://io.streams.TextReader#lines
- * @test  xp://net.xp_framework.unittest.io.streams.LinesTest
+ * @test  xp://net.xp_framework.unittest.io.streams.LinesInTest
  */
-class LinesIn implements \Iterator {
-  const EOF = -1;
-  private $reader, $line, $number, $reset;
+class LinesIn implements \IteratorAggregate {
+  private $reader, $reset;
 
   /**
    * Creates a new lines instance
@@ -30,32 +29,15 @@ class LinesIn implements \Iterator {
     $this->reset= $reset;
   }
 
-  /** @return string */
-  public function current() { return $this->line; }
-
-  /** @return int */
-  public function key() { return $this->number; }
-
-  /** @return bool */
-  public function valid() { return self::EOF !== $this->number; }
-
-  /** @return void */
-  public function next() {
-    if (self::EOF === $this->number) {
-      // Already at EOF, don't attempt further reads
-    } else if (null === ($this->line= $this->reader->readLine())) {
-      $this->number= self::EOF;
-    } else {
-      $this->number++;
-    }
-  }
-
-  /** @return void */
-  public function rewind() {
+  /** @return php.Generator */
+  public function getIterator() {
     if ($this->reset && !$this->reader->atBeginning()) {
       $this->reader->reset();
     }
-    $this->number= 0;
-    $this->next();
+
+    $number= 1;
+    while (null !== ($line= $this->reader->readLine())) {
+      yield $number++ => $line;
+    }
   }
 }
