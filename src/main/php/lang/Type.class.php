a8fdff25f76775fbba4931cb09c16db72402ddae
Timm Friebe <thekid@thekid.de>
2016-04-03 10:52:42 +0200

    QA: Code style

--- src/main/php/lang/Type.class.php
+++ src/main/php/lang/Type.class.php
@@ -62,22 +62,18 @@ class Type extends Object {
   }
 
   /**
-   * Retrieves the fully qualified class name for this class.
+   * Retrieves this type's name
    * 
-   * @return string name - e.g. "io.File", "rdbms.mysql.MySQL"
+   * @return string
    */
-  public function getName() {
-    return $this->name;
-  }
+  public function getName() { return $this->name; }
   
   /**
    * Creates a string representation of this object
    *
    * @return string
    */
-  public function toString() {
-    return nameof($this).'<'.$this->name.'>';
-  }
+  public function toString() { return nameof($this).'<'.$this->name.'>'; }
 
   /**
    * Checks whether a given object is equal to this type
@@ -85,19 +81,15 @@ class Type extends Object {
    * @param  lang.Generic $cmp
    * @return bool
    */
-  public function equals($cmp) {
-    return $cmp instanceof self && $cmp->name === $this->name;
-  }
+  public function equals($cmp) { return $cmp instanceof self && $cmp->name === $this->name; }
 
   /**
    * Returns a hashcode for this object
    *
    * @return string
    */
-  public function hashCode() {
-    return get_class($this).':'.$this->name;
-  }
-  
+  public function hashCode() { return get_class($this).':'.$this->name; }
+
   /**
    * Creates a type list from a given string
    *
@@ -254,9 +246,7 @@ class Type extends Object {
    * @param  var $type
    * @return bool
    */
-  public function isAssignableFrom($type) {
-    return self::$VAR === $this && self::$VOID !== $type;
-  }
+  public function isAssignableFrom($type) { return self::$VAR === $this && self::$VOID !== $type; }
 
   /**
    * Creates a string representation of this object
