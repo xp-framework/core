71d5ca35e0a02bd52378a442850cc8b6633d262e
Timm Friebe <thekid@thekid.de>
2016-08-28 20:42:05 +0200

    Remove return type again, breaks existing code in incompatible ways

--- src/main/php/util/Comparator.class.php
+++ src/main/php/util/Comparator.class.php
@@ -11,6 +11,8 @@ interface Comparator {
    * Compares its two arguments for order. Returns a negative integer, 
    * zero, or a positive integer as the first argument is less than, 
    * equal to, or greater than the second.
+   *
+   * @return int
    */
-  public function compare($a, $b): int;
+  public function compare($a, $b);
 }
