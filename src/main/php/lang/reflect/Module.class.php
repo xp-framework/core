8c66adb93f98151643cc02c1cd5b1bb3df9dc89d
Timm Friebe <thekid@thekid.de>
2017-01-15 17:51:37 +0100

    Implement support for members

--- src/main/php/lang/reflect/Module.class.php
+++ src/main/php/lang/reflect/Module.class.php
@@ -11,23 +11,19 @@ use lang\ElementNotFoundException;
 class Module extends \lang\Object {
   public static $INCOMPLETE= false;
   public static $registered= [];
-  private $definitions;
+  private $name, $classLoader, $definitions;
 
   /**
    * Creates a new module
    *
    * @param  string $name
    * @param  lang.IClassLoader $classLoader
+   * @param  [:php.Closure] $definitions
    */
   public function __construct($name, IClassLoader $classLoader, $definitions= []) {
     $this->name= $name;
     $this->classLoader= $classLoader;
-
-    // Lifecycle: Initialize and finalize, defined as NOOPs by default
-    $this->definitions= ['initialize' => function() { }, 'finalize'   => function() { }];
-    foreach ($definitions as $name => $definition) {
-      $this->definitions[$name]= $definition->bindTo($this, $this);
-    }
+    $this->definitions= array_merge(['initialize' => function() { }, 'finalize' => function() { }], $definitions);
   }
 
   /** @return string */
@@ -36,7 +32,28 @@ class Module extends \lang\Object {
   /** @return lang.IClassLoader */
   public function classLoader() { return $this->classLoader; }
 
-  /** 
+  /**
+   * Member write operator overloading
+   *
+   * @param  string $name
+   * @param  var $value
+   * @return void
+   */
+  public function __set($name, $value) {
+    $this->definitions[$name]= $value;
+  }
+
+  /**
+   * Member read operator overloading
+   *
+   * @param  string $name
+   * @return var
+   */
+  public function __get($name) {
+    return $this->definitions[$name];
+  }
+
+  /**
    * Call operator overloading
    *
    * @param  string $name
@@ -44,7 +61,7 @@ class Module extends \lang\Object {
    * @return var
    */
   public function __call($name, $arguments) {
-    return ($this->definitions[$name])($arguments);
+    return $this->definitions[$name]->call($this, ...$arguments);
   }
 
   /**
