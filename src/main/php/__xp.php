df964721b132a007dc2894d4de23f7d6de77c77b
Timm Friebe <thekid@thekid.de>
2017-06-10 17:29:48 +0200

    Fix HHVM check for hhvm.php7.all

--- src/main/php/__xp.php
+++ src/main/php/__xp.php
@@ -1,13 +1,10 @@
 <?php namespace xp;
 
 if (PHP_VERSION_ID < 70000) {
-  if (!defined('HHVM_VERSION_ID')) {
-    throw new \Exception('This version of the XP Framework requires PHP 7.0.0+, have PHP '.PHP_VERSION);
-  } else if (HHVM_VERSION_ID < 31100) {
-    throw new \Exception('This version of the XP Framework requires HHVM 3.10.0+, have HHVM '.HHVM_VERSION);
-  } else if (!ini_get('hhvm.php7.all')) {
-    throw new \Exception('This version of the XP Framework requires hhvm.php7.all to be set to 1');
-  }
+  throw new \Exception('This version of the XP Framework requires PHP 7.0.0+, have PHP '.PHP_VERSION);
+}
+if (defined('HHVM_VERSION_ID') && !ini_get('hhvm.php7.all')) {
+  throw new \Exception('This version of the XP Framework requires hhvm.php7.all to be set to 1');
 }
 
 $p= max(strrpos(__FILE__, DIRECTORY_SEPARATOR), strrpos(__FILE__, '?'));
