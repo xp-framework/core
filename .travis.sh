6257f5f794c51bfce45b44907e6dee405720471e
Timm Friebe <thekid@thekid.de>
2017-06-10 23:39:10 +0200

    Test with HHVM 3.20.1 and HHVM "latest"

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
 XP_RUNNERS=https://dl.bintray.com/xp-runners/generic/xp-run-master.sh
-HHVM_VERSION=latest
+
+replace_hhvm_with() {
+  local version=$1
+  local wd=$(pwd)
+
+  printf "\033[33;1mReplacing HHVM\033[0m\n"
+
+  echo "hhvm.php7.all = 1" > php.ini
+  echo "hhvm.hack.lang.look_for_typechecker = 0" >> php.ini
+
+  docker pull hhvm/hhvm:$version
+  docker run --rm hhvm/hhvm:$version hhvm --version
+  echo
+
+  printf "\033[33;1mRunning Composer\033[0m\n"
+  cp /home/travis/.phpenv/versions/hhvm/bin/composer composer.in
+  docker run --rm -v $wd:/opt/src -v $wd/php.ini:/etc/hhvm/php.ini -w /opt/src hhvm/hhvm:$version hhvm --php composer.in install
+
+  mv xp-run xp-run.in
+  echo "#!/bin/sh" > xp-run
+  echo "docker run --rm -v $wd:/opt/src -v $wd/php.ini:/etc/hhvm/php.ini -w /opt/src hhvm/hhvm:$version /bin/sh xp-run.in \$@" >> xp-run
+}
 
 case $1 in
   install)
@@ -14,23 +35,12 @@ case $1 in
     # Run HHVM inside Docker as the version provided by Travis-CI is too old
     # For official PHP versions, there's nothing to do
     case "$TRAVIS_PHP_VERSION" in
+      hhvm-nightly*)
+        replace_hhvm_with "latest"
+      ;;
+
       hhvm*)
-        printf "\033[33;1mReplacing HHVM\033[0m\n"
-
-        echo "hhvm.php7.all = 1" > php.ini
-        echo "hhvm.hack.lang.look_for_typechecker = 0" >> php.ini
-
-        docker pull hhvm/hhvm:$HHVM_VERSION
-        docker run --rm hhvm/hhvm:$HHVM_VERSION hhvm --version
-        echo
-
-        printf "\033[33;1mRunning Composer\033[0m\n"
-        cp /home/travis/.phpenv/versions/hhvm/bin/composer composer.in
-        docker run --rm -v $(pwd):/opt/src -v $(pwd)/php.ini:/etc/hhvm/php.ini -w /opt/src hhvm/hhvm:$HHVM_VERSION hhvm --php composer.in install
-
-        mv xp-run xp-run.in
-        echo "#!/bin/sh" > xp-run
-        echo "docker run --rm -v $(pwd):/opt/src -v $(pwd)/php.ini:/etc/hhvm/php.ini -w /opt/src hhvm/hhvm:$HHVM_VERSION /bin/sh xp-run.in \$@" >> xp-run
+        replace_hhvm_with "3.20.1"
       ;;
 
       *)
