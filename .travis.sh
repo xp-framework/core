1293ea055c83acb057191dc820c8e62ee4152136
Timm Friebe <thekid@thekid.de>
2017-06-10 22:54:49 +0200

    Run composer install for HHVM, too

--- .travis.sh
+++ .travis.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
+XP_RUNNERS=https://dl.bintray.com/xp-runners/generic/xp-run-master.sh
+
 case $1 in
   install)
+    echo test.xar > test.pth
+
+    # Run HHVM inside Docker as the version provided by Travis-CI is too old
+    # For official PHP versions, there's nothing to do
     case "$TRAVIS_PHP_VERSION" in
       hhvm*)
-        docker pull hhvm/hhvm:latest
-        docker run --rm hhvm/hhvm:latest hhvm --version
+        curl -sSL $XP_RUNNERS > xp-run.in
 
         echo "hhvm.php7.all = 1" > php.ini
         echo "hhvm.hack.lang.look_for_typechecker = 0" >> php.ini
 
-        mv xp-run xp-run.in
+        docker pull hhvm/hhvm:latest
+        docker run --rm hhvm/hhvm:latest hhvm --version
+
+        cp $(which composer) composer.in
+        docker run --rm -v $(pwd):/opt/src -v $(pwd)/php.ini:/etc/hhvm/php.ini -w /opt/src hhvm/hhvm:latest hhvm --php composer.in install
+
         echo "#!/bin/sh" > xp-run
         echo "docker run --rm -v $(pwd):/opt/src -v $(pwd)/php.ini:/etc/hhvm/php.ini -w /opt/src hhvm/hhvm:latest /bin/sh xp-run.in \$@" >> xp-run
       ;;
 
       *)
+        curl -sSL $XP_RUNNERS > xp-run
         composer install
       ;;
     esac
