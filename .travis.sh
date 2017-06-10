83cf6d20ce84e00211d30ade30c8b8363ad2f970
Timm Friebe <thekid@thekid.de>
2017-06-10 22:17:28 +0200

    Use $TRAVIS_PHP_VERSION

--- .travis.sh
+++ .travis.sh
@@ -2,19 +2,23 @@
 
 case $1 in
   install)
-    if [ -f /etc/hhvm/php.ini ] ; then
-      docker pull hhvm/hhvm:latest
-      docker run --rm hhvm/hhvm:latest hhvm --version
+    case "$TRAVIS_PHP_VERSION" in
+      hhvm*)
+        docker pull hhvm/hhvm:latest
+        docker run --rm hhvm/hhvm:latest hhvm --version
 
-      echo "hhvm.php7.all = 1" > php.ini
-      echo "hhvm.hack.lang.look_for_typechecker = 0" >> php.ini
+        echo "hhvm.php7.all = 1" > php.ini
+        echo "hhvm.hack.lang.look_for_typechecker = 0" >> php.ini
 
-      mv xp-run xp-run.in
-      echo "#!/bin/sh" > xp-run
-      echo "docker run --rm -v $(pwd):/opt/src -v $(pwd)/php.ini:/etc/hhvm/php.ini hhvm/hhvm:latest /bin/sh /opt/src/xp-run.in \$@" >> xp-run
-    else
-      composer install
-    fi
+        mv xp-run xp-run.in
+        echo "#!/bin/sh" > xp-run
+        echo "docker run --rm -v $(pwd):/opt/src -v $(pwd)/php.ini:/etc/hhvm/php.ini -e USE_XP=/opt/src hhvm/hhvm:latest /bin/sh /opt/src/xp-run.in \$@" >> xp-run
+      ;;
+
+      *)
+        composer install
+      ;;
+    esac
   ;;
 
   run-tests)
