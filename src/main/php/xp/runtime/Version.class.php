0a8a266f68027a93670b54babe55453ba96b7fce
Timm Friebe <thekid@thekid.de>
2018-09-29 14:32:00 +0200

    Remove HHVM engine version computation

--- src/main/php/xp/runtime/Version.class.php
+++ src/main/php/xp/runtime/Version.class.php
@@ -1,7 +1,7 @@
 <?php namespace xp\runtime;
 
-use util\cmd\Console;
 use lang\ClassLoader;
+use util\cmd\Console;
 
 /**
  * Displays XP version and runtime information
@@ -20,7 +20,7 @@ class Version {
   private function phpVersion() { return 'PHP/'.phpversion(); }
 
   /** @return string */
-  private function engineVersion() { return defined('HHVM_VERSION') ? 'HHVM/'.HHVM_VERSION : 'Zend/'.zend_version(); }
+  private function engineVersion() { return 'Zend/'.zend_version(); }
 
   /** @return string */
   private function osVersion() {
