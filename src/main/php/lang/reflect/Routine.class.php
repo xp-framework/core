90e08bfa1498c8bfc11d1d233ef5647de03fed15
Timm Friebe <thekid@thekid.de>
2019-06-11 19:24:02 +0200

    Use null-coalesce operator ?? instead of error suppression

--- src/main/php/lang/reflect/Routine.class.php
+++ src/main/php/lang/reflect/Routine.class.php
@@ -192,8 +192,8 @@ class Routine implements Value {
     $details= XPClass::detailsForMethod($this->_reflect->getDeclaringClass(), $this->_reflect->getName());
 
     return $details && ($key 
-      ? array_key_exists($key, (array)@$details[DETAIL_ANNOTATIONS][$name]) 
-      : array_key_exists($name, (array)@$details[DETAIL_ANNOTATIONS])
+      ? array_key_exists($key, $details[DETAIL_ANNOTATIONS][$name] ?? [])
+      : array_key_exists($name, $details[DETAIL_ANNOTATIONS] ?? [])
     );
   }
 
