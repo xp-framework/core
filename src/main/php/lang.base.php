23b11a6b16ab02bd4200d383e303d4f72544d3d9
Timm Friebe <thekid@thekid.de>
2020-09-21 00:18:42 +0200

    Revert accidental change which removed T_NAME_FULLY_QUALIFIED

--- src/main/php/lang.base.php
+++ src/main/php/lang.base.php
@@ -434,8 +434,8 @@ define('MODIFIER_PRIVATE',   \ReflectionMethod::IS_PRIVATE);
 defined('PHP_INT_MIN') || define('PHP_INT_MIN', ~PHP_INT_MAX);
 
 defined('T_FN') || define('T_FN', -346);
-defined('T_FN') || define('T_FN', -346);
 defined('T_ATTRIBUTE') || define('T_ATTRIBUTE', -383);
+defined('T_NAME_FULLY_QUALIFIED') || define('T_NAME_FULLY_QUALIFIED', -312)
 defined('T_NAME_QUALIFIED') || define('T_NAME_QUALIFIED', -314);
 
 xp::$loader= new xp();
