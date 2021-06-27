4f9e3d6e2488fd17718c885add98e13dac55e7f0
Timm Friebe <thekid@thekid.de>
2021-06-27 22:42:33 +0200

    Remove test for PHP_INT_MIN, it was added in PHP 7.0.0

--- src/main/php/lang.base.php
+++ src/main/php/lang.base.php
@@ -462,8 +462,6 @@ define('DETAIL_ANNOTATIONS', 5);
 define('DETAIL_TARGET_ANNO', 6);
 define('DETAIL_GENERIC',     7);
 
-defined('PHP_INT_MIN') || define('PHP_INT_MIN', ~PHP_INT_MAX);
-
 defined('T_FN') || define('T_FN', -346);
 defined('T_ATTRIBUTE') || define('T_ATTRIBUTE', -383);
 defined('T_NAME_FULLY_QUALIFIED') || define('T_NAME_FULLY_QUALIFIED', -312);
