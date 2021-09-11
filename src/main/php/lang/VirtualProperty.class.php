8b3180a01a5e64f4ef5e87681b7844ed684abd92
Timm Friebe <thekid@thekid.de>
2021-09-11 23:47:08 +0200

    Implement remaining ReflectionProperty methods

--- src/main/php/lang/VirtualProperty.class.php
+++ src/main/php/lang/VirtualProperty.class.php
@@ -33,8 +33,8 @@ class VirtualProperty extends ReflectionProperty {
   /** Gets modifiers */
   public function getModifiers(): int { return $this->_meta[0]; }
 
-  /** Gets doc comment */
-  public function getDocComment() { return false; }
+  /** Checks if property has a type */
+  public function hasType(): bool { return isset($this->_meta[1]); }
 
   /** Gets type */
   public function getType(): ReflectionType {
@@ -50,8 +50,42 @@ class VirtualProperty extends ReflectionProperty {
   /** Gets declaring class */
   public function getDeclaringClass(): ReflectionClass { return $this->_class; }
 
+  /** Gets doc comment */
+  #[ReturnTypeWillChange]
+  public function getDocComment() { return false; }
+
+  /** Gets whether a default value is available */
+  public function hasDefaultValue(): bool { return false; }
+
+  /** Gets default value */
+  #[ReturnTypeWillChange]
+  public function getDefaultValue() { return null; }
+
+  /** Checks if property is a default property */
+  public function isDefault(): bool { return false; }
+
   /** Returns whether this property is public */
   public function isPublic(): bool { return MODIFIER_PUBLIC === ($this->_meta[0] & MODIFIER_PUBLIC); }
+
+  /** Returns whether this property is protected */
+  public function isProtected(): bool { return MODIFIER_PROTECTED === ($this->_meta[0] & MODIFIER_PROTECTED); }
+
+  /** Returns whether this property is private */
+  public function isPrivate(): bool { return MODIFIER_PRIVATE === ($this->_meta[0] & MODIFIER_PRIVATE); }
+
+  /** Returns whether this property is private */
+  public function isStatic(): bool { return MODIFIER_STATIC === ($this->_meta[0] & MODIFIER_STATIC); }
+
+  /**
+   * Returns all attributes declared on this class property as an array of ReflectionAttribute.
+   *
+   * @param  ?string $name
+   * @param  int $flags
+   * @return \ReflectionAttribute[]
+   */
+  public function getAttributes(string $name= null, int $flags= 0): array {
+    return [];
+  }
 
   /**
    * Sets accessible flag
@@ -61,6 +95,16 @@ class VirtualProperty extends ReflectionProperty {
    */
   #[ReturnTypeWillChange]
   public function setAccessible($flag) { /* NOOP */ }
+
+  /**
+   * Checks whether a property is initialized.
+   *
+   * @param  ?object $object
+   * @return bool
+   */
+  public function isInitialized($object= null): bool {
+    return null !== $instance->__get($this->_name);
+  }
 
   /**
    * Gets this property's value
