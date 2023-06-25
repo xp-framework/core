eac5dfb1a1533420ade2cf98cd503efb7e3046a7
Timm Friebe <thekid@thekid.de>
2023-06-25 12:40:55 +0200

    Remove ci.bat after successfully inlining it

--- .github/workflows/ci.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-for /f "skip=1 tokens=1-4" %%a in ('findstr /r /c:"^## " ChangeLog.md') do (
-  set COMPOSER_ROOT_VERSION=%%b
-  php .\xp\composer.phar install --prefer-dist
-  echo src/main/php/__xp.php > composer.pth
-  echo vendor/autoload.php >> composer.pth
-  exit /b
-)
