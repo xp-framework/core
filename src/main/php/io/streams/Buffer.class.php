eb2e3f6f6fc2bf361ccc794d54e0e4386460f212
Timm Friebe <thekid@thekid.de>
2024-06-22 18:58:09 +0200

    Implement a limited in-memory buffer with filesystem overflow

---
+++ src/main/php/io/streams/Buffer.class.php
@@ -0,0 +1,116 @@
+<?php namespace io\streams;
+
+use io\{File, Folder};
+use lang\IllegalArgumentException;
+
+/**
+ * Buffers in memory up until a given threshold, using the file system once
+ * it's exceeded.
+ * 
+ * @see   https://github.com/xp-forge/web/issues/118
+ * @test  io.unittest.BufferTest
+ */
+class Buffer implements InputStream, OutputStream {
+  private $files, $threshold;
+  private $memory= '';
+  private $file= null;
+  private $size= 0;
+
+  /**
+   * Creates a new buffer
+   *
+   * @param  io.Folder|io.Path|string $files
+   * @param  int $threshold
+   * @throws lang.IllegalArgumentException
+   */
+  public function __construct($files, int $threshold) {
+    if ($threshold < 0) {
+      throw new IllegalArgumentException('Threshold must be >= 0');
+    }
+
+    $this->files= $files instanceof Folder ? $files->getURI() : (string)$files;
+    $this->threshold= $threshold;
+  }
+
+  /** Returns buffer size */
+  public function size(): int { return $this->size; }
+
+  /** @return ?io.File */
+  public function file() { return $this->file; }
+
+  /**
+   * Write a string
+   *
+   * @param  var $arg
+   * @return void
+   */
+  public function write($bytes) {
+    $this->size+= strlen($bytes);
+    if ($this->size <= $this->threshold) {
+      $this->memory.= $bytes;
+      return;
+    }
+
+    if (null === $this->file) {
+      $this->file= new File(tempnam($this->files, "b{$this->threshold}"));
+      $this->file->open(File::READWRITE);
+      $this->file->write($this->memory);
+      $this->memory= null;
+    }
+    $this->file->write($bytes);
+  }
+
+  /** @return void */
+  public function flush() {
+    $this->file && $this->file->flush();
+  }
+
+  /**
+   * Finish buffering
+   *
+   * @return void
+   */
+  public function finish() {
+    $this->file && $this->file->seek(0, SEEK_SET);
+  }
+
+  /** @return int */
+  public function available() {
+    return $this->file ? $this->size - $this->file->tell() : strlen($this->memory ?? '');
+  }
+
+  /**
+   * Read a string
+   *
+   * @param  int $limit
+   * @return ?string
+   */
+  public function read($limit= 8192) {
+    if ($this->file) {
+      $chunk= $this->file->read($limit);
+      return false === $chunk ? null : $chunk;
+    }
+
+    // Drain memory
+    if (null === $this->memory) {
+      $chunk= null;
+    } else if ($limit >= strlen($this->memory)) {
+      $chunk= $this->memory;
+      $this->memory= null;
+    } else {
+      $chunk= substr($this->memory, 0, $limit);
+      $this->memory= substr($this->memory, $limit);
+    }
+    return $chunk;
+  }
+
+  /** @return void */
+  public function close() {
+    $this->file && $this->file->close();
+  }
+
+  /** Ensure file is closed */
+  public function __destruct() {
+    $this->close();
+  }
+}
---
+++ src/test/php/io/unittest/BufferTest.class.php
@@ -0,0 +1,65 @@
+<?php namespace io\unittest;
+
+use io\File;
+use io\streams\Buffer;
+use lang\{Environment, IllegalArgumentException};
+use test\{Assert, Before, Expect, Test, Values};
+
+class BufferTest {
+  const THRESHOLD= 128;
+  private $temp;
+
+  #[Before]
+  public function temp() {
+    $this->temp= Environment::tempDir();
+  }
+
+  #[Test]
+  public function can_create() {
+    new Buffer($this->temp, self::THRESHOLD);
+  }
+
+  #[Test, Expect(IllegalArgumentException::class)]
+  public function threshold_must_be_larger_than_zero() {
+    new Buffer($this->temp, -1);
+  }
+
+  #[Test, Values([0, 1, 127, 128])]
+  public function uses_memory_under_threshold($length) {
+    $bytes= str_repeat('*', $length);
+
+    $fixture= new Buffer($this->temp, self::THRESHOLD);
+    $fixture->write($bytes);
+    $fixture->finish();
+
+    Assert::null($fixture->file());
+    Assert::equals($length, $fixture->size());
+    Assert::equals($bytes, $fixture->read());
+  }
+
+  #[Test, Values([129, 256, 1024])]
+  public function stores_file_when_exceeding_threshold($length) {
+    $bytes= str_repeat('*', $length);
+
+    $fixture= new Buffer($this->temp, self::THRESHOLD);
+    $fixture->write($bytes);
+    $fixture->finish();
+
+    Assert::instance(File::class, $fixture->file());
+    Assert::equals($length, $fixture->size());
+    Assert::equals($bytes, $fixture->read());
+  }
+
+  #[Test, Values([127, 128, 129])]
+  public function read_after_eof($length) {
+    $bytes= str_repeat('*', self::THRESHOLD + $length);
+
+    $fixture= new Buffer($this->temp, self::THRESHOLD);
+    $fixture->write($bytes);
+    $fixture->finish();
+
+    Assert::equals($bytes, $fixture->read());
+    Assert::equals(0, $fixture->available());
+    Assert::equals(null, $fixture->read());
+  }
+}
