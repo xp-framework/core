20f41ec82407568639a3ff620232b2083c18bac4
Timm Friebe <thekid@thekid.de>
2026-08-08 10:44:06 +0200

    QA: Refactor static member

--- src/test/php/lang/unittest/GenericTypesTest.class.php
+++ src/test/php/lang/unittest/GenericTypesTest.class.php
@@ -4,18 +4,18 @@ use lang\{GenericTypes, Primitive, XPClass};
 use test\{Assert, Before, Test};
 
 class GenericTypesTest {
-  private static $filter;
+  private $filter;
 
   #[Before]
-  public static function defineBase() {
-    self::$filter= XPClass::forName('lang.unittest.ArrayFilter');
+  public function filter() {
+    $this->filter= XPClass::forName('lang.unittest.ArrayFilter');
   }
   
   #[Test]
   public function newType0_returns_literal() {
     Assert::equals(
       "lang\\unittest\\ArrayFilter\xb7\xb7\xfeint",
-      (new GenericTypes())->newType0(self::$filter, [Primitive::$INT])
+      (new GenericTypes())->newType0($this->filter, [Primitive::$INT])
     );
   }
 
@@ -23,14 +23,14 @@ class GenericTypesTest {
   public function newType_returns_XPClass_instance() {
     Assert::instance(
       XPClass::class,
-      (new GenericTypes())->newType(self::$filter, [Primitive::$INT])
+      (new GenericTypes())->newType($this->filter, [Primitive::$INT])
     );
   }
 
   #[Test]
   public function newType_creates_generic_class() {
     Assert::true(
-      (new GenericTypes())->newType(self::$filter, [Primitive::$INT])->isGeneric()
+      (new GenericTypes())->newType($this->filter, [Primitive::$INT])->isGeneric()
     );
   }
 
@@ -38,7 +38,7 @@ class GenericTypesTest {
   public function newType_sets_generic_arguments() {
     Assert::equals(
       [Primitive::$INT],
-      (new GenericTypes())->newType(self::$filter, [Primitive::$INT])->genericArguments()
+      (new GenericTypes())->newType($this->filter, [Primitive::$INT])->genericArguments()
     );
   }
 }
